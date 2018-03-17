5aed4c00d08dfecb3e8fe39da32a0cac70d68187
Igor Rokytskyy <igorritto@gmail.com>
2018-03-17 22:08:45 +0200

    q

--- user/views.py
+++ user/views.py
@@ -91,7 +91,28 @@ def jget_reminders_list(request, id_user):
 
 
 
-'''def generate_reminders():
+'''def generate_reminders(id_user):
+    fav_items = FavoriteItems.objects.filter(user_id=id_user)
+    need_to_create_notification = True
+    for fav_item in fav_items:
+        notifications = Notification.object.filter(favorite_id=fav_item.id)
+        for notification in notifications:
+            if not notification.done:
+                need_to_create_notification = False
+        if need_to_create_notification:
+            n = Notification()
+
+
+
+
+
+
+        // if there is no pending task,
+        // create notification_text
+
+
+
+
 
         fav_item = FavoriteItems.objects.all()
         text = ''
